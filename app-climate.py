bc8ba9a9c9ac5e8290ba6309cf5fbd1ee8d9c346
Victor Yamaykin <victor.yamaykin@gmail.com>
2019-04-11 11:44:52 -0400

    Started flask api

--- app-climate.py
+++ app-climate.py
@@ -24,16 +24,25 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return "Climate analysis<br>\
-        /api/v1.0/precipitation<br>\
-            /api/v1.0/stations<br>\
-                /api/v1.0/tobs<br>\
-                    /api/v1.0/<start><br>\
-                        /api/v1.0/</start>/<end>"
+def welcome():
+    return (
+    f"Welcome to the Hawaii Climate analysis<br>"
+    f"Available Routes<br>"
+    f"<strong>Precipitation Records 2016-2017</strong><br>"
+    f"/api/v1.0/precipitation<br>"
+    f"<strong>Station Names</strong><br>"
+    f"/api/v1.0/stations<br>"
+    f"<strong>Temperature Observations</strong><br>"
+    f"/api/v1.0/tobs<br>"
+    f"<strong>Temperature Low, Average, and High with Start Date: 2017-08-09</strong><br>"
+    f"/api/v1.0/<{start}><br>"
+    f"<strong>Temperature Low, Average, and High with Start Date: 2017-08-09, End Date: 2017-08-23</strong><br>"
+    f"api/v1.0/</{start}>/<{end}>"
+    )
 
 
 @app.route("/api/v1.0/precipitation")
+
 def precipitation():
     year_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
 
@@ -42,19 +51,65 @@ def precipitation():
     group_by(Measurement.date).\
     order_by(Measurement.date.desc()).all()
 
-    df = pd.DataFrame(results, columns=['date', 'precipitation'])
+    prcp = {date: prcp for date, prcp in results}
     
-    return jsonify(df)
-
+    return (
+        jsonify(prcp)
+        )
 
 @app.route("/api/v1.0/stations")
-def jsonified():
-    return jsonify()
+
+def stations():
+    results = session.query(Station.name, Station.station).\
+        group_by(Station.name).\
+        order_by(Station.name.desc()).all()
+    
+    station = {name: station for name, station in results}
+        
+    return (
+        jsonify(station)
+    )
 
 @app.route("/api/v1.0/tobs")
-def jsonify():
-    return jsonify()
 
+def tobs():
+    year_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
+
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date >= year_ago).\
+        group_by(Measurement.date).\
+        order_by(Measurement.date.desc()).all()
+
+    tobs = {tobs: tobs for date, tobs in results}
+
+    return(
+        jsonify(tobs)
+    )
+
+@app.route("/api/v.1.0/<start>")
+
+def calc_temps(start):
+    start = dt.date(2017,8,9) 
+
+    session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+    
+    return (
+        jsonify(calc_temps('2017-08-09')
+    )
+
+@app.route("/api/v1.0/<start>/<end>")
+
+def calc_temps_trip(start, end):
+    start = dt.date(2017,8,9) 
+    end = dt.date(2017,8,23) 
+
+    session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    
+    return (
+        jsonify(calc_temps_trip('2017-08-09', '2017-08-23'))
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
